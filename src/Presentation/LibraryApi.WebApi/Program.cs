2e1bf08afb233734a403aab344ca66e1bac462e3
MustafaIsmayilov <ismayilov06ms@gmail.com>
2025-08-21 00:00:33 +0400

    Part1-Entities

--- src/Presentation/LibraryApi.WebApi/Program.cs
+++ src/Presentation/LibraryApi.WebApi/Program.cs
@@ -1,15 +1,23 @@
+﻿using System;
+using LibraryApi.Persistence.Contexts;  // ✅ DbContext namespace-i
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// ✅ Connection string oxu
+var connectionString = builder.Configuration.GetConnectionString("Default");
 
+// ✅ DbContext-i DI container-ə əlavə et
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Add services to the container.
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
